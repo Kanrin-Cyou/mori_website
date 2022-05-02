8f88f4ddd490d0e77ec475505dfbf022460ffa84
Guanlun <zhaoguanlun@gmail.com>
2022-05-02 17:19:40 +0900

    fix scroll

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import ResponsiveAppBar from './Appbar';
 import Footer from './Footer';
 
 export default function Layout({children}){
 
     return (
-        <div> 
-            <ResponsiveAppBar/>
+        <Box sx={{  overflow:"hidden"}}> 
+            <ResponsiveAppBar sx={{overflow: "hidden"}}/>
             {children}
             <Footer/>
-        </div>
+        </Box>
     )
 }
